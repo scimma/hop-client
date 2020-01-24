8370793ab4ef73bd0d43d4a81f0eaded90feb018
Patrick Godwin <patrick.godwin@psu.edu>
2020-01-24 09:45:48 -0500

    add placeholder test for pytest

---
+++ tests/test_client.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+
+__author__ = "Patrick Godwin (patrick.godwin@psu.edu)"
+__description__ = "a module that tests the client library"
+
+#-------------------------------------------------
+### imports
+
+import pytest
+
+
+#-------------------------------------------------
+### tests
+
+def test_placeholder():
+    pass
