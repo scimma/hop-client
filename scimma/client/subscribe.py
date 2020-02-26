c05dac211211fb98be09795f0663e3af9f05f112
bfc5288 <bfc5288@psu.edu>
2020-02-25 21:17:30 -0500

    Add logic to consume gcn messages with genesis
    Added logic and functions to consume published gcn messages using genesis. Added command options
    for the desired functionality of json dumping, accessing earliest kafka offset, and message timeout.

--- scimma/client/subscribe.py
+++ scimma/client/subscribe.py
@@ -7,7 +7,28 @@ __description__ = "tools to receive and parse GCN circulars"
 import argparse
 import email
 
+import json
+
 from genesis import streaming as stream
+
+#def sub_parse_gcn()
+def print_gcn(gcn_dict,json_dump=False):
+    '''
+    '''
+    if json_dump:
+        print(json.dumps(gcn_dict))
+    else:
+        #print(json.dumps(gcn_dict['header'], indent=0))
+        #print(json.dumps(gcn_dict['body'], indent=0))
+        gcn_headers = gcn_dict['header']
+        gcn_body = gcn_dict['body']
+
+        for key in gcn_headers:
+            print(key, ': ', gcn_headers[key])
+        print('')    #newline for formatting
+
+        for line in gcn_body.splitlines():
+            print(line)
 
 # ------------------------------------------------
 # -- main
@@ -18,10 +39,7 @@ def _add_parser_args(parser):
         "-b",
         "--broker-url",
         required=True,
-        help="Sets the broker URL (kafka://host[:port]/topic) to receive GCNs from.",
-    )
-    parser.add_argument(
-        "-j", "--json", help="A test argument for raw json printing",
+        help="Sets the broker URL (kafka://host[:port]/topic) from which to receive GCNs.",
     )
 
     # configuration options
@@ -36,6 +54,18 @@ def _add_parser_args(parser):
         help="Set client configuration via prop=val. Can be specified multiple times.",
     )
 
+    # consumer options
+    parser.add_argument(
+        "-j", "--json", help="Request gcn output as raw json",
+        action='store_true',
+    )
+    parser.add_argument(
+        "-e", "--earliest", help="Request to stream from the earliest available Kafka offset",
+        action='store_true',
+    )
+    parser.add_argument(
+        "-t", "--timeout", help="Specifies the time (in seconds) to wait for new messages.",
+    )
 
 def _main(args=None):
     """Receive and parse GCN circulars.
@@ -54,3 +84,27 @@ def _main(args=None):
     else:
         config = None
 
+    # load consumer options
+
+    # defaults:
+    start_offset = 'latest'
+    timeout = 10
+    json_dump = False
+
+    if args.json:
+        json_dump = True
+    if args.earliest:
+        start_offset = 'earliest'
+    if args.timeout:
+        timeout = int(args.timeout)
+
+    # read from topic
+
+    # assume json format for the gcn
+    gcn_format = 'json'
+
+    with stream.open(args.broker_url, "r", format=gcn_format, config=config, start_at=start_offset) as s:
+        #gcn_list = [ gcn for _,gcn in s(timeout=timeout, progress=True)]
+        for _,gcn_dict in s(timeout=timeout):
+            print_gcn(gcn_dict,json_dump)
+        
