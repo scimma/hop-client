8ae4529076d339bb8f8ff18ddd1bee4040344442
Patrick Godwin <patrick.godwin@psu.edu>
2020-03-12 22:20:17 -0400

    add test_models.py for testing VOEvent, GCNCircular models

---
+++ tests/test_models.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+
+__author__ = "Patrick Godwin (patrick.godwin@psu.edu)"
+__description__ = "a module that tests models"
+
+
+from scimma.client import models
+
+
+def test_voevent(voevent_fileobj):
+    voevent = models.VOEvent.from_xml(voevent_fileobj)
+
+    # check a few attributes
+    assert voevent.ivorn == "ivo://gwnet/LVC#S200302c-1-Preliminary"
+    assert voevent.role == "observation"
+    assert voevent.version == "2.0"
+
+    assert voevent.Who["Date"] == "2020-03-02T02:00:09"
+    assert voevent.Description == "Report of a candidate gravitational wave event"
+    assert (
+        voevent.WhereWhen["ObsDataLocation"]["ObservatoryLocation"]["id"]
+        == "LIGO Virgo"
+    )
