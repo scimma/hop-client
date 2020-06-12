74799c381714a645219890ddf6c667d5680ba6e6
root <root@euca-10-1-4-211.eucalyptus.internal>
2020-06-12 18:53:28 +0000

    Formatting

--- hop/version.py
+++ hop/version.py
@@ -4,17 +4,19 @@ import confluent_kafka
 __author__ = "Shereen Elsayed (s_elsayed@ucsb.edu"
 __description__ = "Print the versions of the packages that hop-client depends on"
 
+
 def print_packages_versions():
     """ Print versions for the passed packages
 
     """
-    packages=get_packages()
+    packages = get_packages()
     for pkg in packages:
         if pkg == "librdkafka":
-            print("%s==%s" %(pkg, confluent_kafka.libversion()[0]))
+            print("%s==%s" % (pkg, confluent_kafka.libversion()[0]))
         else:
-            print("%s==%s" %(pkg, pkg_resources.get_distribution(pkg).version))
+            print("%s==%s" % (pkg, pkg_resources.get_distribution(pkg).version))
     
+
 def get_packages():
     return ["hop-client", "adc_streaming", "confluent_kafka", "librdkafka"]
 
@@ -27,4 +29,6 @@ def _main(args=None):
     """List all the dependencies' versions
 
     """
+
     print_packages_versions()
+    
