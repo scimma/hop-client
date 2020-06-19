0a4b914ee4d4c1d2647425219ef7b54f09428598
bfc5288 <bfc5288@psu.edu>
2020-06-19 17:51:51 -0400

    Add unit tests reflecting message wrapper and blob format testing

--- tests/test_models.py
+++ tests/test_models.py
@@ -21,6 +21,8 @@ def test_voevent(voevent_fileobj):
     assert voevent.Description == "Report of a candidate gravitational wave event"
     assert voevent.WhereWhen["ObsDataLocation"]["ObservatoryLocation"]["id"] == "LIGO Virgo"
 
+    # verify wrapper format is correct
+    assert voevent.wrap_message()["format"] == "voevent"
 
 def test_gcn_circular(circular_text, circular_msg):
     with patch("builtins.open", mock_open(read_data=circular_text)):
@@ -36,3 +38,18 @@ def test_gcn_circular(circular_text, circular_msg):
         assert gcn.header["from"] == circular_msg["header"]["from"]
 
         assert gcn.body == circular_msg["body"]
+
+        # verify wrapper format is correct
+        assert gcn.wrap_message()["format"] == "circular"
+
+def test_blob(blob_text, blob_msg):
+    with patch("builtins.open", mock_open(read_data=blob_text)) as mock_file:
+        blob_file = "example_blob.txt"
+        with open(blob_file, "r") as f:
+            blob = models.message_blob.from_file(f)
+
+        # verify blob text is correct
+        assert blob.content == blob_msg["content"]
+
+        # verify wrapper format is correct
+        assert blob.wrap_message()["format"] == "blob"
--- tests/test_subscribe.py
+++ tests/test_subscribe.py
@@ -8,21 +8,77 @@ import codecs
 from contextlib import redirect_stdout
 from pathlib import Path
 
-from unittest.mock import patch, MagicMock
+from unittest.mock import patch, MagicMock, Mock, create_autospec
+from dataclasses import fields
 import pytest
 
-from hop import subscribe
+from hop import subscribe, models
+from hop.models import GCNCircular, VOEvent, message_blob
+
+# test the subscribe classifier for each message format
+
+def content_mock(message_model):
+    """Mock content to pass during the message_model creation since MagicMock() 
+    is unable to mock __init__ of the model dataclass in tests.
+    """
+    content = {}
+    for field in fields(message_model):
+        content.update({field.name: "test"})
+    return content
+
+@pytest.mark.parametrize("message", [
+    {"format":"voevent", "content": content_mock(VOEvent)},
+    {"format":"circular", "content": content_mock(GCNCircular)},
+    {"format":"blob", "content": content_mock(message_blob)},
+    {"format":"other", "content": "other"},
+    ])
+def test_classify_message(message):
+    message_format = message["format"]
+    message_content = message["content"]
+
+    if message_format == "voevent":
+        expected_model = models.VOEvent
+        model_name = "VOEvent"
+    elif message_format == "circular":
+        expected_model = models.GCNCircular
+        model_name = "GCNCircular"
+    elif message_format == "blob":
+        expected_model = models.message_blob
+        model_name = "message_blob"
+    else:
+        with pytest.raises(ValueError):
+            test_model = subscribe.classify_message(message)
+        return
+
+    # with patch(f"hop.models.{model_name}", Mock()) as mock_model:
+#    m = MagicMock()
+#    m = content_mock(expected_model)
+#    m.return_value = expected_model
+#    m.__init__ = MagicMock(return_value=expected_model)
+#    with patch(f"hop.models.{model_name}", return_value=m) as patch_model:
+    with patch(f"hop.models.{model_name}", MagicMock()) as patch_model:
+#    with patch.object(f"hop.models.{model_name}", "__init__") as patch_model:
+#        patch_model.__init__.return_value = expected_model
+#        patch_model.return_value = expected_model
+
+        test_model = subscribe.classify_message(message)
+        #patch_model.assert_called_with(message_content)
+
+    assert isinstance(test_model, expected_model)
 
 
-# test the subscribe printer for each message type: voevent, circular
-@pytest.mark.parametrize("msg_type", ["voevent", "circular"])
-def test_print_gcn(msg_type):
-    if msg_type == "voevent":
+# test the subscribe printer for each message format
+@pytest.mark.parametrize("message_format", ["voevent", "circular", "blob"])
+def test_print_message(message_format):
+    if message_format == "voevent":
         model_type = "VOEvent"
         test_file = "example_voevent.xml"
-    elif msg_type == "circular":
+    elif message_format == "circular":
         model_type = "GCNCircular"
         test_file = "example_gcn.gcn3"
+    elif message_format == "blob":
+        model_type = "message_blob"
+        test_file = "example_blob.txt"
 
     shared_datadir = Path("tests/data")
 
@@ -34,24 +90,24 @@ def test_print_gcn(msg_type):
 
         f = io.StringIO()
         with redirect_stdout(f):
-            subscribe.print_gcn(mock_model, json_dump=False)
+            subscribe.print_message(mock_model, json_dump=False)
 
         # read stdout from beginning
         f.seek(0)
 
-        # extract GCN string from stdout
-        test_gcn_stdout_list = f.readlines()
-        test_gcn_stdout_str = "".join(test_gcn_stdout_list)
+        # extract message string from stdout
+        test_message_stdout_list = f.readlines()
+        test_message_stdout_str = "".join(test_message_stdout_list)
 
         # read in expected stdout text
-        expected_gcn_raw = (
+        expected_message_raw = (
             shared_datadir / "expected_data" / (test_file + ".stdout")
         ).read_text()
 
         # use codec to decode the expected output to leave newline/tab characters intact
-        expected_gcn_stdout = codecs.unicode_escape_decode(expected_gcn_raw)[0]
+        expected_message_stdout = codecs.unicode_escape_decode(expected_message_raw)[0]
 
-        # verify printed GCN structure is correct
-        print(test_gcn_stdout_str)
+        # verify printed message structure is correct
+        print(test_message_stdout_str)
 
-        assert test_gcn_stdout_str == expected_gcn_stdout
+        assert test_message_stdout_str == expected_message_stdout
