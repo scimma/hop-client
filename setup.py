035f87b8675fb988bdf3d8f19ce20cc86efd89a2
Patrick Godwin <patrick.godwin@psu.edu>
2020-08-06 18:53:48 -0400

    setup.py, meta.yaml: add pluggy as dependency

--- setup.py
+++ setup.py
@@ -10,6 +10,7 @@ with open(os.path.join(this_dir, 'README.md'), 'rb') as f:
 install_requires = [
     "adc-streaming >= 1.0.2",
     "dataclasses ; python_version < '3.7'",
+    "pluggy >= 0.11",
     "toml >= 0.9.4",
     "xmltodict >= 0.9.0"
 ]
