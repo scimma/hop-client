e2ddae40f3dc297028b64d299ee0851200720c31
shereenelsayed <shereenhussein92@gmail.com>
2020-08-06 19:20:18 +0000

    adding more test points

--- tests/test_auth.py
+++ tests/test_auth.py
@@ -29,15 +29,30 @@ def test_setup_auth():
     with open(credentials_file, "w") as f:
         f.write("username,password\n")
         f.write(username + "," + password + "\n")
+
     # check on new configuration file is written using credential file
-    result = subprocess.Popen(["hop", "configure", "setup", "--import", credentials_file],
-                              stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
-    output, error = result.communicate()
+    process = subprocess.Popen(["hop", "configure", "setup", "--import", credentials_file],
+                               stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+    output, error = process.communicate()
     assert "hop : INFO : Generated configuration at:" in output.decode("utf-8")
     configuration_file = configure.get_config_path()
     cf = open(configuration_file, "r")
     config_file_text = cf.read()
     assert username in config_file_text
     assert password in config_file_text
-
     os.remove(credentials_file)
+
+    # hop configure setup (need --force)
+    process = subprocess.Popen(["hop", "configure", "setup"],
+                               stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+    output, error = process.communicate()
+    warning_message = "hop : WARNING : Configuration already exists, overwrite file with --force"
+    assert warning_message in output.decode("utf-8")
+
+
+def test_no_command_configure():
+    process = subprocess.Popen(["hop", "configure"],
+                               stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+    output, error = process.communicate()
+    warning_message = "hop : WARNING : Please use any of these commands: locate or setup"
+    assert warning_message in output.decode("utf-8")
