0a412b19898ca1c31df4f872f91e755cc0f42532
Cody Messick <cody.messick@ligo.org>
2022-08-31 12:21:58 -0700

    Allow AvroBlob to take either a single record or a list of records

--- tests/conftest.py
+++ tests/conftest.py
@@ -274,7 +274,7 @@ MESSAGE_AVRO = \
     b'\x2e\x33\x42\xea\xa7\x2a\xc4\x52'
 
 # Equivalent to the data encoded in MESSAGE_AVRO
-AVRO_DATA_EQUIVALENT = [{
+AVRO_DATA_EQUIVALENT = {
     "arr": [1, 2, 3, 4],
     "sub_objects": [{"a": 1, "b": 2}, {"c": 3, "d": 4}],
     "thingy": {
@@ -285,7 +285,7 @@ AVRO_DATA_EQUIVALENT = [{
     },
     "data": b"A\x00B\x04",
     "logic": [True, False],
-}]
+}
 
 # This was the original configuration structure, which permitted only a single credential
 AUTH_CONFIG_LEGACY = """\
@@ -532,6 +532,7 @@ def blob_msg():
     return {"content": MESSAGE_BLOB}
 
 
+# avro_data needs to be loaded with single_record=False here because
 message_parameters_dict_data = {
     # Generalize model_name, expected_model, test_file, and model_text
     # for easy access during tests. Useful when combined with parametrization
